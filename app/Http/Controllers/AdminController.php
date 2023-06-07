149233b326875499ec7ec8b4e26ccf8f6fbcd665
parbat-subedi <parbatsubedi000@gmail.com>
2023-06-07 21:56:29 +0545

    added admin authorization for all admin access work only

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,180 +14,280 @@ use Notifications;
 class AdminController extends Controller
 {
     public function addview(){
-        // return 'hello this is api';
-        return view('admin.add_teacher');
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+            return view('admin.add_teacher');
+            }
+        }else{
+            return view('login');
+        }
     }
 
     public function ShowTeacherView(){
-        if(Auth::id()){
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
             $data = teachers::all();
-        return view('admin.Show_Teachers', compact('data'));
+            return view('admin.Show_Teachers', compact('data'));
+            }
         } else{
-        return redirect('home');
+        return redirect('user.home');
         
         }
     }
 
     public function ShowTeacherDetails(){
-        $data=Teachers::paginate(6);
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+            $data=Teachers::paginate(6);
         
-        return view('admin.Teacher_Profile',compact('data'));
+            return view('admin.Teacher_Profile',compact('data'));
+            }
+        } else{
+        return redirect('user.home');
+        }
+
     }
 
     public function upload(Request $request){
-        $teacher= new Teachers;
-        $image=$request->image;
-        $imageName=time().'.'.$image->getClientOriginalExtension();
-        $department = $request->department;
-        $departmentFolder = '';
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $teacher= new Teachers;
+                $image=$request->image;
+                $imageName=time().'.'.$image->getClientOriginalExtension();
+                $department = $request->department;
+                $departmentFolder = '';
 
-        //folder based on the selected department value
-        switch ($department) {
-            case 'HeadOfCampus':
-                $departmentFolder = 'HeadOfCampusFolder';
-                break;
-            case 'IT':
-                $departmentFolder = 'ITFolder';
-                break;
-            case 'Science':
-                $departmentFolder = 'ScienceFolder';
-                break;
-            case 'Management':
-                $departmentFolder = 'ManagementFolder';
-                break;
-            case 'Humanities':
-                $departmentFolder = 'HumanitiesFolder';
-                break;
-            case 'Law':
-                $departmentFolder = 'LawFolder';
-                break;
-            default:
-            //if any of the folders is not found then default is set to default folder
-                $departmentFolder = 'defaultFolder';
+                //folder based on the selected department value
+                switch ($department) {
+                    case 'HeadOfCampus':
+                        $departmentFolder = 'HeadOfCampusFolder';
+                        break;
+                    case 'IT':
+                        $departmentFolder = 'ITFolder';
+                        break;
+                    case 'Science':
+                        $departmentFolder = 'ScienceFolder';
+                        break;
+                    case 'Management':
+                        $departmentFolder = 'ManagementFolder';
+                        break;
+                    case 'Humanities':
+                        $departmentFolder = 'HumanitiesFolder';
+                        break;
+                    case 'Law':
+                        $departmentFolder = 'LawFolder';
+                        break;
+                    default:
+                    //if any of the folders is not found then default is set to default folder
+                        $departmentFolder = 'defaultFolder';
+                }
+                $request->image->move('teacherImage/'.$departmentFolder, $imageName);
+                $path='teacherImage/'.$departmentFolder.'/'.$imageName;
+                // dd($path);
+
+                $teacher->image=$path;
+                // dd($imageName);
+
+                $teacher->name=$request->name;
+                $teacher->phone=$request->phone;
+                $teacher->room=$request->room;
+                $teacher->department=$request->department;
+                $teacher->email=$request->email;
+
+                $teacher->save();
+
+                return redirect()->back()->with('message','Teacher added successfully!');
+            }
+        }else{
+            return redirect('user.home');
         }
-        $request->image->move('teacherImage/'.$departmentFolder, $imageName);
-        $path='teacherImage/'.$departmentFolder.'/'.$imageName;
-        // dd($path);
-
-        $teacher->image=$path;
-        // dd($imageName);
-
-        $teacher->name=$request->name;
-        $teacher->phone=$request->phone;
-        $teacher->room=$request->room;
-        $teacher->department=$request->department;
-
-        $teacher->save();
-
-        return redirect()->back()->with('message','Teacher added successfully!');
 
     }
 
     public function showAppointments(){
-
-        $data=appointments::paginate(6);
-        
-        return view('admin.showappointment',compact('data'));
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data=appointments::paginate(6);
+                
+                return view('admin.showappointment',compact('data'));
+            }
+        }else{
+                return redirect('user.home');
+            }
     }
 
     public function approved($id){
-        $data=appointments::find($id);
-        $data->status='Approved';
-        $data->save();
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data=appointments::find($id);
+                $data->status='Approved';
+                $data->save();
 
-        return redirect()->back();
+                return redirect()->back();
+            }
+        } else{
+                return redirect('user.home');
+                }
     }
 
     public function rejected($id){
-        $data=appointments::find($id);
-        $data->status='Rejected';
-        $data->save();
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data=appointments::find($id);
+                $data->status='Rejected';
+                $data->save();
 
-        return redirect()->back();
+                return redirect()->back();
+            }
+
+        } else{
+                return redirect('user.home');
+        }
     }
 
     public function view($id){
-        $data=teachers::find($id);
-        $data->save();
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data=teachers::find($id);
+                $data->save();
 
-        return view('admin.Teacher_Profile',compact('data'));
+                return view('admin.Teacher_Profile',compact('data'));
+            }
+        }else{
+                return redirect('user.home');
+            }
     }
 
     public function delete($id){
-        $data = teachers::find($id);
-        $data->delete();
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data = teachers::find($id);
+                $data->delete();
 
-        return redirect()->back();
+                return redirect()->back();
+            }
+        }else{
+            return redirect('user.home');
+        }
     }
 
     public function update(Request $request,$id){
-        $data = teachers::find($id);
-        
-        return view('admin.Update_Profile',compact('data'));
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data = teachers::find($id);
+                
+                return view('admin.Update_Profile',compact('data'));
+            }
+        }else{
+            return redirect('user.home');
+        }
     }
 
     public function editTeacher(Request $request,$id){
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $teacher= teachers::find($id);
 
-        $teacher= teachers::find($id);
+                
+                $teacher->name=$request->name;
+                $teacher->phone=$request->phone;
+                $teacher->room=$request->room;
+                $teacher->department=$request->department;
 
-        
-        $teacher->name=$request->name;
-        $teacher->phone=$request->phone;
-        $teacher->room=$request->room;
-        $teacher->department=$request->department;
+                $image=$request->image;
+                $department = $request->department;
+                $departmentFolder = '';
 
-        $image=$request->image;
-        $department = $request->department;
-        $departmentFolder = '';
+                //folder based on the selected department value
+                switch ($department) {
+                    case 'HeadOfCampus':
+                        $departmentFolder = 'HeadOfCampusFolder';
+                        break;
+                    case 'IT':
+                        $departmentFolder = 'ITFolder';
+                        break;
+                    case 'Science':
+                        $departmentFolder = 'ScienceFolder';
+                        break;
+                    case 'Management':
+                        $departmentFolder = 'ManagementFolder';
+                        break;
+                    case 'Humanities':
+                        $departmentFolder = 'HumanitiesFolder';
+                        break;
+                    case 'Law':
+                        $departmentFolder = 'LawFolder';
+                        break;
+                    default:
+                    //if any of the folders is not found then default is set to default folder
+                        $departmentFolder = 'defaultFolder';
+                }
+                if($image){
+                $imageName=time().'.'.$image->getClientOriginalExtension();
 
-        //folder based on the selected department value
-        switch ($department) {
-            case 'HeadOfCampus':
-                $departmentFolder = 'HeadOfCampusFolder';
-                break;
-            case 'IT':
-                $departmentFolder = 'ITFolder';
-                break;
-            case 'Science':
-                $departmentFolder = 'ScienceFolder';
-                break;
-            case 'Management':
-                $departmentFolder = 'ManagementFolder';
-                break;
-            case 'Humanities':
-                $departmentFolder = 'HumanitiesFolder';
-                break;
-            case 'Law':
-                $departmentFolder = 'LawFolder';
-                break;
-            default:
-            //if any of the folders is not found then default is set to default folder
-                $departmentFolder = 'defaultFolder';
-        }
-        if($image){
-        $imageName=time().'.'.$image->getClientOriginalExtension();
+                $request->image->move('teacherImage/'.$departmentFolder, $imageName);
+                $path='teacherImage/'.$departmentFolder.'/'.$imageName;
+                // dd($path);
 
-        $request->image->move('teacherImage/'.$departmentFolder, $imageName);
-        $path='teacherImage/'.$departmentFolder.'/'.$imageName;
-        // dd($path);
-
-        $teacher->image=$path;
-        // dd($imageName);
-        }
+                $teacher->image=$path;
+                // dd($imageName);
+                }
 
 
-        $teacher->save();
-        return redirect()->back()->with('message','Teacher data updated successfully!');
+                $teacher->save();
+                return redirect()->back()->with('message','Teacher data updated successfully!');
+            }
+        }else{
+                return redirect('user.home');
+            }
     }
 
     public function showUser(){
-        $user = User::paginate(6);
-        return view('admin.Show_Users',compact('user'));
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $user = User::paginate(6);
+                return view('admin.Show_Users',compact('user'));
+            }
+        } else{
+            return redirect('user.home');
+        }
     }
 
     public function emailview($id){
-        $data= appointments::find($id);
-        return view('admin.email_view',compact('data'));
+        if (Auth::id())
+        {
+            if(Auth::user()->usertype =='1')
+            {
+                $data= appointments::find($id);
+                return view('admin.email_view',compact('data'));
+            }
+        }else{
+            return redirect('user.home');
+        }
     }
 
     public function sendEmail(Request $request,$id){
